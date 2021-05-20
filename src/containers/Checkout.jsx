462e53a5932aa153fa1e23348457976aaa1e7897
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2021-05-20 08:24:14 -0500

    Aplicacion funcionando solo cambiar en Paymente los paypaloptions y agregas los api key de google.

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/Checkout.css';
+import AppContext from '../context/AppContext';
 
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+  const total =
+    cart.length > 0
+      ? cart.reduce((acumulador, numero) => acumulador + numero.price, 0)
+      : 0;
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         <h3>Lista de Pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>ITEM name</h4>
-            <span>$10</span>
+        {cart.map((item, index) => (
+          <div key={index} className="Checkout-item">
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
+            <button type="button" onClick={() => removeFromCart(item)}>
+              <i className="fas fa-trash-alt" />
+            </button>
           </div>
-          <button type="button">
-            <i className="fas fa-trash-alt" />
-          </button>
-        </div>
+        ))}
       </div>
       <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
+        <h3>Precio Total: ${total}</h3>
         <Link to="/checkout/information">
           <button type="button">Continuar pedido</button>
         </Link>
       </div>
     </div>
   );
-}
+};
 
 export default Checkout;
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import initialState from '../InitialState';
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+  const addToCart = (payload) => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  };
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((item) => item.id !== payload.id),
+    });
+  };
+  const addToBuyer = (payload) => {
+    setState({
+      ...state,
+      buyer: [...state.buyer, payload],
+    });
+  };
+  const addNewOrder = (payload) => {
+    setState({
+      ...state,
+      orders: [...state.orders, payload],
+    });
+  };
+  return {
+    addToCart,
+    removeFromCart,
+    addToBuyer,
+    addNewOrder,
+    state,
+  };
+};
+export default useInitialState;
